aace18da85cd8fc0393d0ac59ea00c8a871ffadd
Igor Boyko <iboyko@gmail.com>
2022-10-24 12:00:43 -0700

    added row col location

--- src/index.js
+++ src/index.js
@@ -53,6 +53,17 @@ function Square(props) {
     }
 
     handleClick(i) {
+        const locations = [
+            [1,1],
+            [1,2],
+            [1,3],
+            [2,1],
+            [2,2],
+            [2,3],
+            [3,1],
+            [3,2],
+            [3,3]
+        ];
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
@@ -62,7 +73,8 @@ function Square(props) {
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
                 history: history.concat([{
-                squares: squares
+                squares: squares,
+                location: locations[i] // added
             }]),
             stepNumber: history.length,
             xIsNext: !this.state.xIsNext
@@ -82,7 +94,9 @@ function Square(props) {
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
-            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            const desc = move 
+            ? `Move #${move} : Row${history[move].location[0]}, Col${history[move].location[1]}` 
+            : 'Go to game start';
             return (
                 <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
